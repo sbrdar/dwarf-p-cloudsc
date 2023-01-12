dad979a613842235fca020a5ac506d69095a30b6
Slavko Brdar <slavko.brdar@ecmwf.int>
2023-01-12 17:05:56 +0100

    move atlas stuff out of the expand module

--- src/common/module/expand_atlas_mod.F90
+++ src/common/module/expand_atlas_mod.F90
@@ -7,23 +7,24 @@
 ! granted to it by virtue of its status as an intergovernmental organisation
 ! nor does it submit to any jurisdiction.
 
-module expand_mod
+module expand_atlas_mod
+  use atlas_module
+  use atlas_fieldset_module
+  use atlas_functionspace_blockstructuredcolumns_module
+
   use parkind1 , only : jpim, jprb
   use yomphyder, only : state_type
 
   use cloudsc_mpi_mod, only : irank, numproc
   use file_io_mod, only: input_initialize, load_scalar, load_array
 
+  use, intrinsic :: iso_c_binding, only : c_int, c_long, c_float, c_double, c_bool
+
   implicit none
 
   interface expand
      procedure expand_l1, expand_i1, expand_r1, expand_r2, expand_r3
   end interface expand
-
-  interface load_and_expand
-     procedure load_and_expand_l1, load_and_expand_i1
-     procedure load_and_expand_r1, load_and_expand_r2, load_and_expand_r3
-  end interface load_and_expand
 
 contains
 
@@ -45,129 +46,128 @@ contains
     end = start + size - 1
   end subroutine get_offsets
 
-  subroutine load_and_expand_i1(name, field, nlon, nproma, ngptot, nblocks, ngptotg)
+  subroutine loadvar_atlas(fset, name, nlon, ngptotg)
     ! Load into the local memory buffer and expand to global field
-    character(len=*) :: name
-    integer(kind=jpim), allocatable, intent(inout) :: field(:,:)
-    integer(kind=jpim), intent(in) :: nlon, nproma, ngptot, nblocks
+    type(atlas_fieldset), intent(inout) :: fset
+    character(len=*), intent(in) :: name
+    integer(kind=jpim), intent(in) :: nlon
     integer(kind=jpim), intent(in), optional :: ngptotg
-    integer(kind=jpim), allocatable :: buffer(:)
-    integer(kind=jpim) :: start, end, size
 
-    call get_offsets(start, end, size, nlon, 1, 1, ngptot, ngptotg)
-    if (.not. allocated(field))  allocate(field(nproma, nblocks))
-    allocate(buffer(size))
-    call load_array(name, start, end, size, nlon, buffer)
-    call expand(buffer, field, size, nproma, ngptot, nblocks)
-    deallocate(buffer)
-  end subroutine load_and_expand_i1
+    integer(kind=jpim) :: start, end, size, nlev, nproma, ngptot, nblocks, ndim, frank
+    type(atlas_field) :: field
+    real(kind=jprb), allocatable :: buffer_r1(:), buffer_r2(:,:), buffer_r3(:,:,:)
+    integer(kind=jpim), allocatable :: buffer_i1(:)
+    logical, allocatable :: buffer_l1(:)
+    real(c_double), pointer :: field_r1(:,:), field_r2(:,:,:), field_r3(:,:,:,:)
+    integer(c_int), pointer :: field_i1(:,:)
+    logical, pointer :: field_l1(:,:)
+    type(atlas_functionspace_blockstructuredcolumns) :: fspace
+    logical :: lfield, rfield, ifield
 
-  subroutine load_and_expand_l1(name, field, nlon, nproma, ngptot, nblocks, ngptotg)
+    field = fset%field(name)
+    frank = field%rank()
+    lfield = (name == "LDCUM")
+    ifield = (name == "KTYPE")
+    rfield = ((.not. lfield) .and. (.not. ifield))
+
+    fspace = field%functionspace()
+    nlev = field%levels()
+    nproma = fspace%block_size(1)
+    ngptot = fspace%size()
+    nblocks = fspace%nblks()
+
+    if (frank == 2) then
+      call get_offsets(start, end, size, nlon, 1, 1, ngptot, ngptotg)
+      if (rfield) then
+        allocate(buffer_r1(size))
+        call field%data(field_r1)
+        call load_array(name, start, end, size, nlon, buffer_r1)
+        call expand(buffer_r1, field_r1, size, nproma, ngptot, nblocks)
+        deallocate(buffer_r1)
+      else if (lfield) then
+        allocate(buffer_l1(size))
+        call field%data(field_l1)
+        call load_array(name, start, end, size, nlon, buffer_l1)
+        call expand(buffer_l1, field_l1, size, nproma, ngptot, nblocks)
+        deallocate(buffer_l1)
+      else
+        allocate(buffer_i1(size))
+        call field%data(field_i1)
+        call load_array(name, start, end, size, nlon, buffer_i1)
+        call expand(buffer_i1, field_i1, size, nproma, ngptot, nblocks)
+        deallocate(buffer_i1)
+      endif
+    else if (frank == 3) then
+      call get_offsets(start, end, size, nlon, 1, nlev, ngptot, ngptotg)
+      if (rfield) then
+        call field%data(field_r2)
+        allocate(buffer_r2(size, nlev))
+        call load_array(name, start, end, size, nlon, nlev, buffer_r2)
+        call expand(buffer_r2, field_r2, size, nproma, nlev, ngptot, nblocks)
+        deallocate(buffer_r2)
+      endif
+    else if (frank == 4) then
+      ndim = field%shape(3)
+      call get_offsets(start, end, size, nlon, ndim, nlev, ngptot, ngptotg)
+      if (rfield) then
+        call field%data(field_r3)
+        allocate(buffer_r3(size, nlev, ndim))
+        call load_array(name, start, end, size, nlon, nlev, ndim, buffer_r3)
+        call expand(buffer_r3, field_r3, size, nproma, nlev, ndim, ngptot, nblocks)
+        deallocate(buffer_r3)
+      endif
+    endif
+  end subroutine loadvar_atlas
+
+  subroutine loadstate_atlas(fset, name, state, nlon, ngptotg)
     ! Load into the local memory buffer and expand to global field
-    character(len=*) :: name
-    logical, allocatable, intent(inout) :: field(:,:)
-    integer(kind=jpim), intent(in) :: nlon, nproma, ngptot, nblocks
-    integer(kind=jpim), intent(in), optional :: ngptotg
-    logical, allocatable :: buffer(:), rbuf(:)
-    integer(kind=jpim) :: start, end, size
-    integer(kind=4), allocatable :: tmp(:)
-
-    call get_offsets(start, end, size, nlon, 1, 1, ngptot, ngptotg)
-    if (.not. allocated(field))  allocate(field(nproma, nblocks))
-    allocate(buffer(size))
-    call load_array(name, start, end, size, nlon, buffer)
-    call expand(buffer, field, size, nproma, ngptot, nblocks)
-    deallocate(buffer)
-  end subroutine load_and_expand_l1
-
-  subroutine load_and_expand_r1(name, field, nlon, nproma, ngptot, nblocks, ngptotg)
-    ! Load into the local memory buffer and expand to global field
-    character(len=*) :: name
-    real(kind=JPRB), allocatable, intent(inout) :: field(:,:)
-    integer(kind=jpim), intent(in) :: nlon, nproma, ngptot, nblocks
-    integer(kind=jpim), intent(in), optional :: ngptotg
-    real(kind=jprb), allocatable :: buffer(:)
-    integer(kind=jpim) :: start, end, size
-
-    call get_offsets(start, end, size, nlon, 1, 1, ngptot, ngptotg)
-    if (.not. allocated(field))  allocate(field(nproma, nblocks))
-    allocate(buffer(size))
-    call load_array(name, start, end, size, nlon, buffer)
-    call expand(buffer, field, size, nproma, ngptot, nblocks)
-    deallocate(buffer)
-  end subroutine load_and_expand_r1
-
-  subroutine load_and_expand_r2(name, field, nlon, nlev, nproma, ngptot, nblocks, ngptotg)
-    ! Load into the local memory buffer and expand to global field
-    character(len=*) :: name
-    real(kind=JPRB), allocatable, intent(inout) :: field(:,:,:)
-    integer(kind=jpim), intent(in) :: nlon, nlev, nproma, ngptot, nblocks
-    integer(kind=jpim), intent(in), optional :: ngptotg
-    real(kind=jprb), allocatable :: buffer(:,:)
-    integer(kind=jpim) :: start, end, size
-
-    call get_offsets(start, end, size, nlon, 1, nlev, ngptot, ngptotg)
-    if (.not. allocated(field))  allocate(field(nproma, nlev, nblocks))
-    allocate(buffer(size, nlev))
-    call load_array(name, start, end, size, nlon, nlev, buffer)
-    call expand(buffer, field, size, nproma, nlev, ngptot, nblocks)
-    deallocate(buffer)
-  end subroutine load_and_expand_r2
-
-  subroutine load_and_expand_r3(name, field, nlon, nlev, ndim, nproma, ngptot, nblocks, ngptotg)
-    ! Load into the local memory buffer and expand to global field
-    character(len=*) :: name
-    real(kind=JPRB), allocatable, intent(inout) :: field(:,:,:,:)
-    integer(kind=jpim), intent(in) :: nlon, nlev, ndim, nproma, ngptot, nblocks
-    integer(kind=jpim), intent(in), optional :: ngptotg
-    real(kind=jprb), allocatable :: buffer(:,:,:)
-    integer(kind=jpim) :: start, end, size
-
-    call get_offsets(start, end, size, nlon, ndim, nlev, ngptot, ngptotg)
-    if (.not. allocated(field))  allocate(field(nproma, nlev, ndim, nblocks))
-    allocate(buffer(size, nlev, ndim))
-    call load_array(name, start, end, size, nlon, nlev, ndim, buffer)
-    call expand(buffer, field, size, nproma, nlev, ndim, ngptot, nblocks)
-    deallocate(buffer)
-  end subroutine load_and_expand_r3
-
-  subroutine load_and_expand_state(name, state, field, nlon, nlev, ndim, nproma, ngptot, nblocks, ngptotg)
-    ! Load into the local memory buffer and expand to global field
+    type(atlas_fieldset), intent(inout) :: fset
     character(len=*) :: name
     type(state_type), allocatable, intent(inout) :: state(:)
-    real(kind=JPRB), allocatable, target, intent(inout) :: field(:,:,:,:)
-    integer(kind=jpim), intent(in) :: nlon, nlev, ndim, nproma, ngptot, nblocks
+    integer(kind=jpim), intent(in) :: nlon
     integer(kind=jpim), intent(in), optional :: ngptotg
-    real(kind=jprb), allocatable :: buffer(:,:,:)
-    integer(kind=jpim) :: start, end, size
 
     integer :: b
+    integer(kind=jpim) :: start, end, size, nlev, nproma, ngptot, nblocks, ndim
+    type(atlas_field) :: field
+    type(atlas_functionspace_blockstructuredcolumns) :: fspace
+
+    real(kind=jprb), allocatable :: buffer(:,:,:)
+    real(c_double), pointer :: field_r3(:,:,:,:)
+
+    field = fset%field(name)
+    fspace = field%functionspace()
+    nlev = field%levels()
+    nproma = fspace%block_size(1)
+    ngptot = fspace%size()
+    nblocks = fspace%nblks()
+    ndim = field%shape(3) - 3
 
     call get_offsets(start, end, size, nlon, ndim, nlev, ngptot, ngptotg)
-    if (.not. allocated(state))  allocate(state(nblocks))
-    if (.not. allocated(field))  allocate(field(nproma, nlev, 3+ndim, nblocks))
     allocate(buffer(size, nlev, 3+ndim))
+    if (.not. allocated(state))  allocate(state(nblocks))
+    call field%data(field_r3)
 
     call load_array(name//'_T', start, end, size, nlon, nlev, buffer(:,:,1))
     call load_array(name//'_A', start, end, size, nlon, nlev, buffer(:,:,2))
     call load_array(name//'_Q', start, end, size, nlon, nlev, buffer(:,:,3))
     call load_array(name//'_CLD', start, end, size, nlon, nlev, ndim, buffer(:,:,4:))
 
-    call expand(buffer(:,:,1), field(:,:,1,:), size, nproma, nlev, ngptot, nblocks)
-    call expand(buffer(:,:,2), field(:,:,2,:), size, nproma, nlev, ngptot, nblocks)
-    call expand(buffer(:,:,3), field(:,:,3,:), size, nproma, nlev, ngptot, nblocks)
-    call expand(buffer(:,:,4:), field(:,:,4:,:), size, nproma, nlev, ndim, ngptot, nblocks)
+    call expand(buffer(:,:,1), field_r3(:,:,1,:), size, nproma, nlev, ngptot, nblocks)
+    call expand(buffer(:,:,2), field_r3(:,:,2,:), size, nproma, nlev, ngptot, nblocks)
+    call expand(buffer(:,:,3), field_r3(:,:,3,:), size, nproma, nlev, ngptot, nblocks)
+    call expand(buffer(:,:,4:), field_r3(:,:,4:,:), size, nproma, nlev, ndim, ngptot, nblocks)
     deallocate(buffer)
 
 !$OMP PARALLEL DO DEFAULT(SHARED), PRIVATE(B) schedule(runtime)
     do b=1, nblocks
-       state(b)%t => field(:,:,1,b)
-       state(b)%a => field(:,:,2,b)
-       state(b)%q => field(:,:,3,b)
-       state(b)%cld => field(:,:,4:3+ndim,b)
+       state(b)%t => field_r3(:,:,1,b)
+       state(b)%a => field_r3(:,:,2,b)
+       state(b)%q => field_r3(:,:,3,b)
+       state(b)%cld => field_r3(:,:,4:3+ndim,b)
     end do
 !$OMP end parallel do
-  end subroutine load_and_expand_state
+  end subroutine loadstate_atlas
 
   subroutine expand_l1(buffer, field, nlon, nproma, ngptot, nblocks)
     logical, intent(inout) :: buffer(nlon), field(nproma, nblocks)
@@ -333,4 +333,4 @@ contains
 !$omp end parallel do
   end subroutine expand_r3
 
-end module expand_mod
+end module expand_atlas_mod
